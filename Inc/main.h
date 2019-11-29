a0c8ac42d5a9d4bc42731fe9a79f10c6329e162e
Jacques MONNIER <monnier.jacques.lgm@gmail.com>
2019-11-29 16:28:26 +0100

    Add all sources after generation

---
+++ Inc/main.h
@@ -0,0 +1,112 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32l1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define USER_BUTTON_Pin GPIO_PIN_13
+#define USER_BUTTON_GPIO_Port GPIOC
+#define USER_BUTTON_EXTI_IRQn EXTI15_10_IRQn
+#define USART_TX_Pin GPIO_PIN_2
+#define USART_TX_GPIO_Port GPIOA
+#define USART_RX_Pin GPIO_PIN_3
+#define USART_RX_GPIO_Port GPIOA
+#define BTN4_Pin GPIO_PIN_5
+#define BTN4_GPIO_Port GPIOC
+#define BTN4_EXTI_IRQn EXTI9_5_IRQn
+#define LED1_Pin GPIO_PIN_1
+#define LED1_GPIO_Port GPIOB
+#define LED2_Pin GPIO_PIN_2
+#define LED2_GPIO_Port GPIOB
+#define LED3_Pin GPIO_PIN_10
+#define LED3_GPIO_Port GPIOB
+#define LED4_Pin GPIO_PIN_11
+#define LED4_GPIO_Port GPIOB
+#define BTN3_Pin GPIO_PIN_6
+#define BTN3_GPIO_Port GPIOC
+#define BTN3_EXTI_IRQn EXTI9_5_IRQn
+#define PWM_BUZZ_Pin GPIO_PIN_7
+#define PWM_BUZZ_GPIO_Port GPIOC
+#define NSS_Pin GPIO_PIN_8
+#define NSS_GPIO_Port GPIOA
+#define BTN1_Pin GPIO_PIN_11
+#define BTN1_GPIO_Port GPIOA
+#define BTN1_EXTI_IRQn EXTI15_10_IRQn
+#define BTN2_Pin GPIO_PIN_12
+#define BTN2_GPIO_Port GPIOA
+#define BTN2_EXTI_IRQn EXTI15_10_IRQn
+#define TMS_Pin GPIO_PIN_13
+#define TMS_GPIO_Port GPIOA
+#define TCK_Pin GPIO_PIN_14
+#define TCK_GPIO_Port GPIOA
+#define SWO_Pin GPIO_PIN_3
+#define SWO_GPIO_Port GPIOB
+#define PWM_ENGINE_Pin GPIO_PIN_4
+#define PWM_ENGINE_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
